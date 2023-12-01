59a06d6fb342afeeb79b2871855447c4bc47cecd
mostafa21314 <mostafa21314@aucegypt.edu>
2023-12-01 02:01:54 +0200

    improving the functions

--- Assignment_5_Applied_Data_Structures/Source.cpp
+++ Assignment_5_Applied_Data_Structures/Source.cpp
@@ -4,6 +4,7 @@
 #include"Edge.h"
 #include<fstream>
 #include<string>
+
 using namespace std;
 
 double FilterData(string unfiltered)
@@ -12,7 +13,56 @@ double FilterData(string unfiltered)
 	filtered = stod(unfiltered);
 	return filtered;
 }
+void downHeap(Node* arr[], int current, string type,int size)
+{
+	if ((current * 2 >= size) || ((current * 2 + 1) >= size))
+		return;
+	if ((arr[current * 2]->date == "") && (arr[current * 2 + 1]->date == ""))
+	{
+		return;
+	}
+	if (type == "min")
+	{
+		if ((arr[current * 2]->exchangeRate < arr[current]->exchangeRate) || (arr[current * 2 + 1]->exchangeRate < arr[current]->exchangeRate))
+		{
+			if ((arr[current * 2]->exchangeRate) < (arr[current * 2 + 1]->exchangeRate))
+			{
+				Node temp = *arr[current];
+				*arr[current] = *arr[current * 2];
+				*arr[current * 2] = temp;
+				downHeap(arr, current * 2, type,size);
+			}
+			else
+			{
+				Node* temp = arr[current];
+				arr[current] = arr[current * 2 + 1];
+				arr[current * 2 + 1] = temp;
+				downHeap(arr, current * 2 + 1, type,size);
+			}
+		}
 
+	}
+	else
+	{
+		if ((arr[current * 2]->exchangeRate > arr[current]->exchangeRate) || (arr[current * 2 + 1]->exchangeRate > arr[current]->exchangeRate))
+		{
+			if (arr[current * 2]->exchangeRate > arr[current * 2 + 1]->exchangeRate)
+			{
+				Node* temp = arr[current];
+				arr[current] = arr[current * 2];
+				arr[current * 2] = temp;
+				downHeap(arr, current * 2, type,size);
+			}
+			else
+			{
+				Node* temp = arr[current];
+				arr[current] = arr[current * 2 + 1];
+				arr[current * 2 + 1] = temp;
+				downHeap(arr, current * 2 + 1, type,size);
+			}
+		}
+	}
+}
 void upHeap(Node* arr[], int current, string type)
 {
 	if (type == "min")
@@ -81,14 +131,14 @@ void Build_Min_Heap(Node* arr[], int size, string fileName)   // builds a minimu
    }
    average = average / (counter-1);
    cout << "The average is " << average <<endl;
-   for (int i = 1; i < size - 1; i++)
+   for (int i = 1; i < size ; i++)
    {
 	   arr[i]->exchangeRate -= average;
    }
-  for (int i = 1; i < size-1; i++)
+  /*for (int i = 1; i < size-1; i++)
    {
 	   cout << arr[i]->exchangeRate << endl;
-   }
+   }*/
  
 }
 
@@ -123,7 +173,7 @@ void Build_Max_Heap(Node* arr[], int size, string fileName)   // builds a maximu
 	}
 	average = average / (counter - 1);
 	cout << "The average is " << average << endl;
-	for (int i = 1; i < size - 1; i++)
+	for (int i = 1; i < size; i++)
 	{
 		arr[i]->exchangeRate -= average;
 	}
@@ -144,9 +194,28 @@ void insert(Node* arr[], int counter, int size, Node value)
 
 }
 
-void downHeap(Node* arr[])
+Node deleteNode(Node* arr[], int &current, string type,int size)
 {
+	Node temp = *arr[1];
+	*arr[1] = *arr[current];
+	arr[current]->date = ""; arr[current--]->exchangeRate = 0;
+downHeap(arr, 1, type,size);
+	return temp;
 
+
+}
+
+void arrangedValues(Node* arr[],int size, int cursor, int k, string type)
+{
+	if (type == "min")
+		cout << "The minimum values are : " << endl;
+	else
+		cout << "The maximum values are : " << endl;
+	for (int i = 0; i < k; i++)
+	{
+		Node tempor = deleteNode(arr, cursor, type, size);
+		cout << tempor.date << "  " << tempor.exchangeRate << endl;
+	}
 }
 
 int main()
@@ -160,5 +229,18 @@ int main()
 	string temp = "/Users/mosta/source/repos/Assignment_5_Applied_Data_Structures/Assignment_5_Applied_Data_Structures/euro-dollar.txt";
 	Build_Min_Heap(minheap, SIZE, temp);
 	Build_Max_Heap(maxheap, SIZE, temp);
+	int cursorMax = SIZE-1;
+	int cursorMin = SIZE - 1;
+	//for(int i=1;i<SIZE;i++)
+	//{
+	//	Node tempor = deleteNode(minheap, cursor, "min", SIZE);
+	//	cout << tempor.date << "  " << tempor.exchangeRate << endl;
+	//}
+	arrangedValues(maxheap, SIZE, cursorMax, 10, "max");
+	arrangedValues(minheap, SIZE, cursorMin, 10, "min");
+	// deleteNode(minheap, cursor, "min")->exchangeRate;
+	//eleteNode(minheap, cursor, "min")->exchangeRate;
+
+
 
 }
